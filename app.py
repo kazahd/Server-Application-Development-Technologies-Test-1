a2185072199962b7cd3831551080bfc0c39e1a3d
kazahd <tihonsemenov123@gmail.com>
2026-02-25 14:55:10 +0300

    .gitignore, comments

--- app.py
+++ app.py
@@ -1,25 +1,28 @@
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse, FileResponse
+from fastapi.responses import FileResponse
 from models import User, Feedback
 from feedback_data import feedbacks
 
 application = FastAPI()
 
+#1.1
 @application.get("/")
 async def read_root():
     return {"message": "Добро пожаловать в моё приложение FastAPI!"}
 
+#1.2
 @application.get("/index", response_class=FileResponse)
 async def get_html_page():
     return "index.html"
 
+#1.4
 @application.get("/users", response_model=User)
 async def get_user():
-    example_user = User(name="Ваше Имя и Фамилия", id=1)
+    example_user = User(name="Семенов Тихон", id=1)
     return example_user
 
+#2.1
 @application.post("/feedback")
 async def create_feedback(feedback: Feedback):
     feedbacks.append(feedback)
-    
     return {"message": f"Feedback received. Thank you, {feedback.name}."}
--- models.py
+++ models.py
@@ -1,9 +1,9 @@
 from pydantic import BaseModel
-
+#1.4
 class User(BaseModel):
     name: str
     id: int
-
+#2.1
 class Feedback(BaseModel):
     name: str
     message: str
