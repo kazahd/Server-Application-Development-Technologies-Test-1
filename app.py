11df5c09d288a8210e5981f51987eb1ff7b633f0
kazahd <tihonsemenov123@gmail.com>
2026-02-25 13:49:18 +0300

    task 1.2

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
 application = FastAPI()
 
 @application.get("/")
 async def read_root():
     return {"message": "Добро пожаловать в моё приложение FastAPI!"}
+
+@application.get("/index", response_class=FileResponse)
+async def get_html_page():
+    return "index.html"
